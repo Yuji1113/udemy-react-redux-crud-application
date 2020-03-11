cf6c5e6fc46a2121982f52a2e48e1ddf9b35fcd0
mbp <mbp@mbp.local>
2020-03-11 15:40:48 +0900

    Learn props

--- src/App.js
+++ src/App.js
@@ -11,19 +11,36 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    {
+      name:"Taro",
+      age:10
+    },
+    {
+      age:5
+    },
+    {
+      name:"Jiro"
+    }
+  ]
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile,index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <h1>Meow!</h1>
+const User = (props) => {
+return <h2>I am {props.name}! and {props.age} years old!</h2>
 }
 
+User.defaultProps = {
+  name: "Taro",
+  age: 10
+}
 
 export default App;
