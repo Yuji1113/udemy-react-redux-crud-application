9427b01f3b752c6a9b6b73faa7e4e5395ab09a58
mbp <mbp@mbp.local>
2020-03-11 15:27:39 +0900

    create class compornents functional compornents

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React,{Component} from 'react';
-
-const greeting = "Hi,Tanaka";
-class App extends Component{
-  render(){
-    return (
-    <React.Fragment>
-      <h1 className="foo">{greeting}</h1>
-      <input type="text" onChange={() => {console.log("I am Clicked!")}} className="foo" />
-    </React.Fragment>
-    )
-  }
-}
-// }
+import React from 'react';
 
 // class App extends Component{
 //   render(){
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello World!!"
+//     return (
+//     <React.Fragment>
+//       <input type="text" onChange={() => {console.log("I am Clicked!")}} className="foo" />
+//     </React.Fragment>
 //     )
 //   }
 // }
+
+const App = () => {
+  return(
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <h1>Meow!</h1>
+}
+
 
 export default App;
