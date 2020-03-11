604912aba4cdedf460a6f42da3ee88fd751154a0
mbp <mbp@mbp.local>
2020-03-11 16:57:17 +0900

    implment action action-creator

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// export const increment = () => {
+//   return {
+//     type:"INCREMENT"
+//   }
+// }
+
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+
+export const increment = () => {{
+    type: INCREMENT
+}}
+export const decrement = () => {{
+    type: DECREMENT
+}}
